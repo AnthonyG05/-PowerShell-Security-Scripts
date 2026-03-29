2f3ed0caa17b659590c4e993413d1c67d3a60569
AnthonyG05 <anthonyguzman05.ag@gmail.com>
2026-03-29 05:25:06 -0400

    Create Base64-Encoding-&-Decoding-in-PowerShell.ps1

---
+++ Base64-Encoding-&-Decoding-in-PowerShell.ps1
@@ -0,0 +1,33 @@
+# A:
+function EncodeToBase64 {
+
+    $PlainText = Get-Content "\InPut" -Raw
+    
+    $encoding = [System.Text.Encoding]::Unicode.GetBytes( $PlainText)
+    $EncodedString = [System.Convert]::ToBase64String($encoding)
+    
+    $EncodedString |
+        Out-File -FilePath "\Outputs\" 
+    
+}
+EncodeToBase64
+
+# B:
+function DecodeFromBase64  {
+        $original = Get-Content "\InPut" -Raw
+        $EncodedString = Get-Content "\Outputs\" -Raw
+        
+        $decoded = [System.Convert]::FromBase64String($EncodedString)
+        $DecodedPlainText = [System.Text.Encoding]::Unicode.GetString($decoded)
+        
+        Write-Host $DecodedPlainText
+        
+        if($DecodedPlainText -eq $original) {
+            Write-Host "Decoded words are the same as the original"
+        } 
+        else {
+            Write-Host "Decoded words are not the same as the original"
+        }
+    
+}
+DecodeFromBase64
